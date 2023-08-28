be695d4c555ca89caca606189c771658abbd225f
0914888762 <nguyenngoctuankiet1209@gmail.com>
2023-08-28 16:19:25 +0700

    kiet

---
+++ todolist/src/components/TodoList.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import Todo from './Todo'
+import TodoForm from './TodoForm'
+
+function TodoList() {
+    const [todos, setTodos] = useState([])
+
+    const addTodo = todo => {
+        if (!todo.text || /^\s*$/.test(todo.text)) {
+            return
+        }
+
+        const newTodos = [todo, ...todos]
+
+        setTodos(newTodos)
+        console.log(...todos)
+    }
+
+
+
+
+  return (
+    <>
+        <h1>What's the Plan for Today?</h1>
+        <TodoForm onSubmit={addTodo} />
+        <Todo
+        todos={todos}
+        />
+    </>
+  )
+}
+
+export default TodoList
+
+
